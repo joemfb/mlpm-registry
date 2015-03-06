d7e9afefaad7a6900acaf2f1f34dc2d92e45770a
Joseph Bryan <joemfb@gmail.com>
2015-03-06 03:42:47 -0500

    cleans up /account page, adds functionality to claim system modules

--- ui/app/user/account-ctrl.js
+++ ui/app/user/account-ctrl.js
@@ -4,19 +4,57 @@
   angular.module('mlpm.user')
     .controller('AccountCtrl', AccountCtrl);
 
-  AccountCtrl.$inject = ['$scope', 'User', '$location'];
+  AccountCtrl.$inject = ['$scope', 'User', '$location', 'MLRest'];
 
-  function AccountCtrl($scope, user, $location) {
+  function AccountCtrl($scope, user, $location, mlRest) {
     var model = {
-      user: user
+      user: user,
+      systemPackages: [],
+      alerts: []
     };
 
-    if ( user.loaded && !user.authenticated ) {
-      $location.path('/');
+    (function init() {
+      $scope.$watch('model.user.loaded', function(newValue) {
+        if ( user.loaded && !user.authenticated ) {
+          $location.path('/');
+        }
+
+        if (user.authenticated) {
+          getSystemPackages();
+        }
+      });
+    })();
+
+    function getSystemPackages() {
+      mlRest.extension('system-packages', {
+        params: { 'rs:username': user.username }
+      }).then(function(response) {
+        model.systemPackages = response.data;
+      });
+    }
+
+    function claimPackage(packageObj) {
+      mlRest.extension('system-packages', {
+        method: 'POST',
+        params: {
+          'rs:uri': packageObj.uri,
+          'rs:username': user.username
+        }
+      }).then(function(response) {
+        model.alerts.push({
+          type: 'success',
+          msg: 'you\'ve successfully claimed ' + packageObj.name + '!'
+        })
+        getSystemPackages();
+      });
     }
 
     angular.extend($scope, {
-      model: model
+      model: model,
+      claimPackage: claimPackage,
+      closeAlert: function closeAlert(index) {
+        model.alerts.splice(index, 1);
+      }
     });
   }
 
