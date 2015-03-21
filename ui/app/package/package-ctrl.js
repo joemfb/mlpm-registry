30d7774a3ed1710cd44b13a58da44df42391e4e3
Joseph Bryan <joemfb@gmail.com>
2015-03-21 02:20:55 -0400

    improves display of packages with no/short READMEs

--- ui/app/package/package-ctrl.js
+++ ui/app/package/package-ctrl.js
@@ -8,15 +8,23 @@
 
   function PackageCtrl($scope, $http, $routeParams, $sce) {
     var model = {
-      detail: {}
+      detail: {},
+      shortReadme: false,
+      readme: null
     };
 
     $http.get('/api/package/' + $routeParams.package).then(function(response) {
       model.detail = response.data;
-      model.readme = $sce.trustAsHtml(model.detail['parsed-readme']);
       model.detail.dependencies = _.map(model.detail.dependencies, function(version, dependency) {
         return { version: version, name: dependency };
       });
+
+      if (model.detail['parsed-readme']) {
+        model.shortReadme = model.detail['parsed-readme'].length < 500;
+        model.readme = $sce.trustAsHtml(model.detail['parsed-readme']);
+      } else {
+        model.shortReadme = true;
+      }
     });
 
     angular.extend($scope, {
